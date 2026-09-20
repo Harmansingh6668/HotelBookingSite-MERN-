4f5d50766a198b1288f6f52cdfaa335bda9626f6
roop12072005 <kamalroop101@gmail.com>
2026-09-20 14:08:04 +0530

    DNS resolved

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
+const dns = require("dns");
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const connectDatabase = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
